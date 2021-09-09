75394b3ac71df399d0e633465f25903b39dc3524
Roma <moshkovcevroma@gmail.com>
2021-09-09 14:54:27 +0700

    Initial Commit

---
+++ ListOfFilms/ListOfFilmsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ListOfFilmsApp.swift
+//  ListOfFilms
+//
+//  Created by Роман Мошковцев on 09.09.2021.
+//
+
+import SwiftUI
+
+@main
+struct ListOfFilmsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
